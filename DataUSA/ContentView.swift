06908768d91398c29de012b44509b441ac0280b8
Leonardo Soares <leonardofs88@live.com>
2024-09-01 22:30:21 +0100

    Initial Commit

---
+++ DataUSA/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DataUSA
+//
+//  Created by Leonardo Soares on 01/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DataUSA/DataUSAApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataUSAApp.swift
+//  DataUSA
+//
+//  Created by Leonardo Soares on 01/09/24.
+//
+
+import SwiftUI
+
+@main
+struct DataUSAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
